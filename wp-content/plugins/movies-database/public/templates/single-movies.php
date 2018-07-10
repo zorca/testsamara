a33caab388d2d2c7d935ea47336eccf27ab69922
Zorca <vs@zorca.org>
2018-07-10 14:59:02 +0300

    Output taxonomies for single movie

--- wp-content/plugins/movies-database/public/templates/single-movies.php
+++ wp-content/plugins/movies-database/public/templates/single-movies.php
@@ -12,7 +12,55 @@ get_header(); ?>
 
             <?php while ( have_posts() ) : the_post(); ?>
 
-                <?php get_template_part( 'content', 'single' ); ?>
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <header class="entry-header page-header">
+
+                        <?php
+                        if ( of_get_option( 'single_post_image', 1 ) == 1 ) :
+                            the_post_thumbnail( 'unite-featured', array( 'class' => 'thumbnail' ));
+                        endif;
+                        ?>
+
+                        <h1 class="entry-title "><?php the_title(); ?></h1>
+
+                        <div class="entry-meta">
+                            <?php unite_posted_on(); ?>
+                        </div><!-- .entry-meta -->
+                    </header><!-- .entry-header -->
+
+                    <div class="entry-content">
+                        <?php the_content(); ?>
+                        <?php
+                        wp_link_pages( array(
+                            'before' => '<div class="page-links">' . __( 'Pages:', 'unite' ),
+                            'after'  => '</div>',
+                        ) );
+                        ?>
+                    </div><!-- .entry-content -->
+
+                    <footer class="entry-meta">
+                        <?php
+                        /* translators: used between list items, there is a space after the comma */
+                        $genres_list = get_the_term_list( $post->ID, 'genres', 'Жанры: ', ',', '' );
+                        $countries_list = get_the_term_list( $post->ID, 'countries', 'Страна: ', ',', '' );
+                        $years_list = get_the_term_list( $post->ID, 'years', 'Год выпуска: ', ',', '' );
+
+                        $meta_text = '<i class="fa fa-folder-open-o"></i> %1$s. <i class="fa fa-folder-open-o"></i> %2$s. <i class="fa fa-folder-open-o"></i> %3$s. <i class="fa fa-link"></i> <a href="%4$s" rel="bookmark">permalink</a>.';
+
+                        printf(
+                            $meta_text,
+                            $genres_list,
+                            $countries_list,
+                            $years_list,
+                            get_permalink()
+                        );
+                        ?>
+
+                        <?php edit_post_link( __( 'Edit', 'unite' ), '<i class="fa fa-pencil-square-o"></i><span class="edit-link">', '</span>' ); ?>
+                        <?php unite_setPostViews(get_the_ID()); ?>
+                        <hr class="section-divider">
+                    </footer><!-- .entry-meta -->
+                </article><!-- #post-## -->
 
                 <?php unite_post_nav(); ?>
 
