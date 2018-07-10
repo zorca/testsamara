3240be510815e67d445571bd7d32bfa4f717933d
Zorca <vs@zorca.org>
2018-07-10 16:14:23 +0300

    Shortcode fixed with standard wp function

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -1,13 +1,19 @@
 <?php
 
 function database_movies_list( $atts ){
-    $movies_list_query = new WP_Query('post_type=movies');
+    $recent_movies_list = wp_get_recent_posts(
+        array(
+            'post_type'   => 'movies',
+            'post_status' => 'publish',
+            'numberposts' => 5,
+        ));
     $output = '<ul>';
-    while ( $movies_list_query->have_posts() ) : $movies_list_query->the_post();
-        $output .= '<h3><a class="title" href="' . apply_filters( 'the_permalink', get_permalink() ) . '">' . get_the_title() . '</a></h3>';
-    endwhile;
+    foreach( $recent_movies_list as $movie ){
+        setup_postdata( $movie );
+        $output .= '<li><a href="' . get_permalink($movie['ID'])  . '">' . $movie['post_title'] . '</a></li>';
+    }
     $output .= '</ul>';
-    wp_reset_query();
+    wp_reset_postdata();
     return $output;
 }
 add_shortcode( 'database-movies-list', 'database_movies_list' );
